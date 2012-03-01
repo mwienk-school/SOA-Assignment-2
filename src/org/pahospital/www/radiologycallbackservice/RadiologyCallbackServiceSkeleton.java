933766749148691a950594dd3fb5ca5bfc993dbe
Mark Wienk <markwienk@gmail.com>
2012-03-01 16:13:58 +0100

    Callback service stubs

---
+++ src/org/pahospital/www/radiologycallbackservice/RadiologyCallbackServiceSkeleton.java
@@ -0,0 +1,32 @@
+
+/**
+ * RadiologyCallbackServiceSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.1  Built on : Aug 31, 2011 (12:22:40 CEST)
+ */
+    package org.pahospital.www.radiologycallbackservice;
+    /**
+     *  RadiologyCallbackServiceSkeleton java skeleton for the axisService
+     */
+    public class RadiologyCallbackServiceSkeleton implements RadiologyCallbackServiceSkeletonInterface{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param radiologyReport0 
+             * @return  
+         */
+        
+                 public void sendRadiologyReport
+                  (
+                  org.pahospital.www.radiologycallbackservice.RadiologyReport radiologyReport0
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                
+        }
+     
+    }
+    
