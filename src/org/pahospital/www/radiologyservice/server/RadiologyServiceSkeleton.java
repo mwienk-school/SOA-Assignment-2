965e709fd076168617be72f01aca072705243ab2
Mark Wienk <markwienk@gmail.com>
2012-03-02 12:38:15 +0100

    Implementation for the Radiology Server

--- src/org/pahospital/www/radiologyservice/server/RadiologyServiceSkeleton.java
+++ src/org/pahospital/www/radiologyservice/server/RadiologyServiceSkeleton.java
@@ -6,65 +6,106 @@
  */
 package org.pahospital.www.radiologyservice.server;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.xml.soap.SOAPException;
+
+import org.pahospital.www.radiologyservice.*;
+
 /**
  * RadiologyServiceSkeleton java skeleton for the axisService
  */
-public class RadiologyServiceSkeleton implements
-		RadiologyServiceSkeletonInterface {
+public class RadiologyServiceSkeleton implements RadiologyServiceSkeletonInterface {
+	//Keep track of the RadiologyOrder objects with their IDs, statuses and payments
+	protected HashMap<RadiologyOrderID,RadiologyOrder> orderList = new HashMap<RadiologyOrderID,RadiologyOrder>();
+	protected int maxOrderId = 0;
+	protected HashMap<RadiologyOrderID,OrderStatus> orderStatuses = new HashMap<RadiologyOrderID, OrderStatus>();
+	protected HashMap<RadiologyOrderID,Boolean> payments = new HashMap<RadiologyOrderID, Boolean>();
+	//Keep track of the Appointments
+	protected ArrayList<Appointment> appointments = new ArrayList<Appointment>();
+	
 
 	/**
-	 * Auto generated method signature
+	 * Request an appointment for a radiology exam.
 	 * 
-	 * @param appointment2
-	 * @return appointment3
+	 * @param appointment the request
+	 * @return the appointment
+	 * @throws SOAPException 
 	 */
 
-	public org.pahospital.www.radiologyservice.Appointment requestAppointment(
-			org.pahospital.www.radiologyservice.Appointment appointment2) {
-		// TODO : fill this with the necessary business logic
-		throw new java.lang.UnsupportedOperationException("Please implement "
-				+ this.getClass().getName() + "#requestAppointment");
+	public Appointment requestAppointment(Appointment appointment) throws SOAPException {
+		if(orderList.containsKey(appointment.getOrderID())) {
+			//Insert the appointment
+			appointments.add(appointment);
+			//Change OrderStatus
+			orderStatuses.get(appointment.getOrderID()).setOrderStatus("appointment made");
+			return appointment;
+		} else {
+			throw new SOAPException("Radiology order ID unknown, please provide an existing order ID.");
+		}
 	}
 
 	/**
-	 * Auto generated method signature
+	 * Get the status of an radiology order.
 	 * 
-	 * @param radiologyOrderID4
-	 * @return orderStatus5
+	 * @param radiologyOrderID The radiology order id
+	 * @return orderStatus the status object of an order
+	 * @throws SOAPException 
 	 */
-
-	public org.pahospital.www.radiologyservice.OrderStatus getOrderStatus(
-			org.pahospital.www.radiologyservice.RadiologyOrderID radiologyOrderID4) {
-		// TODO : fill this with the necessary business logic
-		throw new java.lang.UnsupportedOperationException("Please implement "
-				+ this.getClass().getName() + "#getOrderStatus");
+	public OrderStatus getOrderStatus(RadiologyOrderID radiologyOrderID) throws SOAPException {
+		if(orderStatuses.containsKey(radiologyOrderID)) {
+			return orderStatuses.get(radiologyOrderID);
+		} else {
+			throw new SOAPException("Radiology order ID unknown, please provide an existing order ID.");
+		}
 	}
 
 	/**
-	 * Auto generated method signature
+	 * Make a payment for a radiology order
 	 * 
-	 * @param radiologyOrderIDForPayment6
+	 * @param radiologyOrderIDForPayment
 	 * @return
+	 * @throws SOAPException 
 	 */
 
-	public void makePayment(
-			org.pahospital.www.radiologyservice.RadiologyOrderIDForPayment radiologyOrderIDForPayment6) {
-		// TODO : fill this with the necessary business logic
-
+	public void makePayment(RadiologyOrderIDForPayment radiologyOrderIDForPayment) throws SOAPException {
+		//Convert RadiologyOrderIDForPayment to RadiologyOrderID
+		RadiologyOrderID id = new RadiologyOrderID();
+		id.setRadiologyOrderID(radiologyOrderIDForPayment.getRadiologyOrderIDForPayment());
+		
+		//Set RadiologyOrder as paid
+		if(!payments.get(id)) {
+			payments.put(id,true);
+		} else {
+			throw new SOAPException("This order has already been paid");
+		}
 	}
 
 	/**
-	 * Auto generated method signature
+	 * Request an radiology examination.
 	 * 
-	 * @param radiologyOrder7
-	 * @return radiologyOrderID8
+	 * @param radiologyOrder the radiology order
+	 * @return radiologyOrderID the radiology order ID
+	 * @throws SOAPException 
 	 */
-
-	public org.pahospital.www.radiologyservice.RadiologyOrderID orderRadiologyExamination(
-			org.pahospital.www.radiologyservice.RadiologyOrder radiologyOrder7) {
-		// TODO : fill this with the necessary business logic
-		throw new java.lang.UnsupportedOperationException("Please implement "
-				+ this.getClass().getName() + "#orderRadiologyExamination");
+	public RadiologyOrderID orderRadiologyExamination(RadiologyOrder radiologyOrder) throws SOAPException {
+		RadiologyOrderID id = new RadiologyOrderID();
+		id.setRadiologyOrderID(String.valueOf(this.maxOrderId));
+		if(!this.orderList.containsKey(id)) {
+			//Insert the order
+			this.orderList.put(id,radiologyOrder);
+			//Insert the status
+			OrderStatus status = new OrderStatus();
+			status.setOrderID(id.getRadiologyOrderID());
+			status.setOrderStatus("ordered");
+			this.orderStatuses.put(id, status);
+			//Insert the payment
+			payments.put(id, false);
+			//Increment the id
+			this.maxOrderId++;
+			return id;
+		}
+		throw new SOAPException("Internal error: RadiologyOrderID exists.");
 	}
 
 }
--- src/org/pahospital/www/radiologyservice/server/RadiologyServiceSkeletonInterface.java
+++ src/org/pahospital/www/radiologyservice/server/RadiologyServiceSkeletonInterface.java
@@ -1,71 +1,53 @@
-
 /**
  * RadiologyServiceSkeletonInterface.java
  *
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.6.1  Built on : Aug 31, 2011 (12:22:40 CEST)
  */
-    package org.pahospital.www.radiologyservice.server;
-    /**
-     *  RadiologyServiceSkeletonInterface java skeleton interface for the axisService
-     */
-    public interface RadiologyServiceSkeletonInterface {
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                    * @param appointment
-         */
+package org.pahospital.www.radiologyservice.server;
 
-        
-                public org.pahospital.www.radiologyservice.Appointment requestAppointment
-                (
-                  org.pahospital.www.radiologyservice.Appointment appointment
-                 )
-            ;
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                    * @param radiologyOrderID
-         */
+import org.pahospital.www.radiologyservice.*;
+import javax.xml.soap.SOAPException;
 
-        
-                public org.pahospital.www.radiologyservice.OrderStatus getOrderStatus
-                (
-                  org.pahospital.www.radiologyservice.RadiologyOrderID radiologyOrderID
-                 )
-            ;
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                    * @param radiologyOrderIDForPayment
-         */
+/**
+ * RadiologyServiceSkeletonInterface java skeleton interface for the axisService
+ */
+public interface RadiologyServiceSkeletonInterface {
 
-        
-                public void makePayment
-                (
-                  org.pahospital.www.radiologyservice.RadiologyOrderIDForPayment radiologyOrderIDForPayment
-                 )
-            ;
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                    * @param radiologyOrder
-         */
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param appointment
+	 * @throws SOAPException 
+	 */
 
-        
-                public org.pahospital.www.radiologyservice.RadiologyOrderID orderRadiologyExamination
-                (
-                  org.pahospital.www.radiologyservice.RadiologyOrder radiologyOrder
-                 )
-            ;
-        
-         }
-    
+	public Appointment requestAppointment(Appointment appointment) throws SOAPException;
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param radiologyOrderID
+	 * @throws SOAPException 
+	 */
+
+	public OrderStatus getOrderStatus(RadiologyOrderID radiologyOrderID) throws SOAPException;
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param radiologyOrderIDForPayment
+	 * @throws SOAPException 
+	 */
+
+	public void makePayment(RadiologyOrderIDForPayment radiologyOrderIDForPayment) throws SOAPException;
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param radiologyOrder
+	 * @throws SOAPException
+	 */
+
+	public RadiologyOrderID orderRadiologyExamination(RadiologyOrder radiologyOrder) throws SOAPException;
+
+}
